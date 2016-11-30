920f5bf0a82ca7cfb79c33cc766cc86872eedb9f
Fzekun <10939721@qq.com>
2016-11-30 09:55:09 +0800

    update

---
+++ src/pages/index/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by fengzekun on 16/11/29.
+ */
+require('../../css/index.css');
+
+document.write('<h1>Hello world</h1>');
+$('body').append('<h2>终于配好jquery了</h2>');
---
+++ src/pages/list/list.js
@@ -0,0 +1,5 @@
+/**
+ * Created by fengzekun on 16/11/29.
+ */
+require('../../css/list.css');
+document.write('<h1>Hello list</h1>')
